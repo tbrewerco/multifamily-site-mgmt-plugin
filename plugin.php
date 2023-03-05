d35b74960ad637b6e52d614f92489dc69e439ff2
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-05 11:19:11 -0700

    Initialize new plugin with metadata

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Multi-family Site Management Plugin
+ * Plugin URI: 
+ * Description: A plugin that imports unit data from the Sightmap API and creates custom post types for each unit in WordPress. The plugin includes an admin page where users can trigger the API import process and view imported unit data, as well as a shortcode to display a styled list of unit posts on the front end of the site.
+ * Version: 1.0.0
+ * Author: Travis Brewer
+ * Author URI: 
+ * License: 
+ */
